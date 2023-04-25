c6fd34e7223140dff5d1482710e2f6d6892e6b5c
jiduslove <oondayr@gamil.com>
2023-04-25 18:02:38 +0900

    today commit

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,10 +1,66 @@
+import Web3 from "web3";
 import Intro from "../components/Intro";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../web3.config";
+import Nfts from "../components/Nfts";
+import { useEffect, useState } from "react";
 
-const Main = () => {
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+const Main = ({ account }) => {
+  const [totalNft, setTotalNft] = useState(0);
+  const [mintedNft, setMintedNft] = useState(0);
+  const [myNft, setMyNft] = useState(0);
+  const [page, setPage] = useState(1);
+
+  const getTotalNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalNft().call();
+
+      setTotalNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMintedNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalSupply().call();
+
+      setMintedNft(response);
+      setPage(parseInt((parseInt(response) - 1) / 2) + 1); //나중에 3을 10으로 수정.
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMyNft = async () => {
+    try {
+      if (!contract || !account) return;
+
+      const response = await contract.methods.balanceOf(account).call();
+
+      setMyNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTotalNft();
+    getMintedNft();
+  }, []);
+  useEffect(() => {
+    getMyNft();
+  }, [account]);
+
   return (
-    <div>
-      <Intro />
-    </div>
+    <>
+      <Intro totalNft={totalNft} mintedNft={mintedNft} myNft={myNft} />
+      <Nfts page={page} />
+    </>
   );
 };
 
